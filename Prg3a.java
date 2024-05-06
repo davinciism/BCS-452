f0597c7e2b40e89de73f4aca738659df0e0f2ab4
Pratik Kuril <pratikkuril16@gmail.com>
2024-05-07 03:53:45 +0530

    Implementation of program demonstrating the use case of a static variable

---
+++ Prg3a.java
@@ -0,0 +1,40 @@
+/*
+To write inputs program that show implementation of
+1. static variables and static member functions
+2. static block concept
+*/
+
+class StudentsInAClass{
+    static int count = 0;
+    //Try running the program without the static keyword above
+    String name = "";
+    int rollno = 0;
+    StudentsInAClass(String name){
+        this.name = name;
+        this.rollno = count+1;
+        count++;
+    }
+    void display(){
+        System.out.println("Name : "+this.name);
+        System.out.println("Roll Number. : "+this.rollno);
+    }
+}
+
+public class Prg3a {
+    public static void main(String args[]){
+        System.out.println("Students Details :-");
+        System.out.println("");
+        StudentsInAClass std1 = new StudentsInAClass("Pratik Kuril");
+        std1.display();
+        StudentsInAClass std2 = new StudentsInAClass("Ishaan patel");
+        std2.display();
+        StudentsInAClass std3 = new StudentsInAClass("Piyush Sharma");
+        std3.display();
+        StudentsInAClass std4 = new StudentsInAClass("Mehak Singh");
+        std4.display();
+        System.out.println("");
+        System.out.println("Total Students : "+ std4.count);
+    }
+}
+
+
